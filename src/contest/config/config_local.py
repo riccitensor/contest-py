04fc6dc684d4d0c12d6aa5405a6650bfae7e2f58
Christian Winkelmann <christian.winkelmann@plista.com>
2012-05-30 11:12:12 +0200

    added config files

---
+++ src/contest/config/config_local.py
@@ -0,0 +1,20 @@
+'''
+Created on 04.12.2011
+
+config file which holds local changeable configuration data like server names, ports etc
+
+@author: christian.winkelmann@plista.com
+'''
+
+config_redis_server_basic = "localhost"
+config_rabbitmq_server = "localhost"
+
+cassandra_hosts = ['46.51.176.41', 'localhost'] # this is for pooling, but its not implemented in python cql and I am too lazy to do that myself
+
+cassandra_host = 'localhost'
+cassandra_port = 9160
+
+
+mysql_host = '192.168.1.34'
+mysql_user = 'karisu'
+mysql_password = '12345'
