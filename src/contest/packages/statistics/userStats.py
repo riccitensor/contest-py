5a644092acf9af9ede9a7280f9449f6f5f913a85
Christian Winkelmann <christian.winkelmann@plista.com>
2012-06-03 13:29:28 +0200

    UserStats remoddeling

--- src/contest/packages/statistics/userStats.py
+++ src/contest/packages/statistics/userStats.py
@@ -25,10 +25,10 @@ statistic for impressions
 
 
 
-		#self.redisConnection.zincrby('userid:item' + str(userid), itemid, 1) #
-		#self.redisConnection.expire('userid:' + str(userid), 172800) # two days ttl
+	#self.redisConnection.zincrby('userid:item' + str(userid), itemid, 1) #
+	#self.redisConnection.expire('userid:' + str(userid), 172800) # two days ttl
 
-		#self.redis_con.rpush(self.userid_location, userid) # just log all user ids
+	#self.redis_con.rpush(self.userid_location, userid) # just log all user ids
 
 	def save(self, indexid, dimensionValue):
 
@@ -36,7 +36,7 @@ statistic for impressions
 		self.redisConnection.sadd(self.main_key + ":{}:list".format(self.dimension_Name), dimensionValue )
 
 		#self.redisConnection.zadd(self.main_key + ":{}:zset".format(self.dimension_Name), )
-		self.redisConnection.zincrby(self.main_key + ":zset:".format(self.index_Name) , indexid, 1) #
+		self.redisConnection.zincrby(self.main_key + ":zset:{}".format(self.index_Name) , indexid, 1) #
 
 
 	def computeKey(self, index, payload):
@@ -44,19 +44,18 @@ statistic for impressions
 
 
 	def get_Top_N(self, dimension_Name, N):
-		key = self.main_key + ":{}:list".format(dimension_Name)
-		#toplist = self.redisConnection.zrevrangebyscore(key, float("infinity"), 0, 0, num=N, withscores = True)
+		key = self.main_key + ":zset:{}".format(dimension_Name)
+		toplist = self.redisConnection.zrevrangebyscore(key, float("infinity"), 0, 0, num=N, withscores = True)
 		#self.redisConnection.zrangebyscore(key, start=1, )
-		self.redisConnection.zrevrangebyscore("bla", 4, 1, start=0, num=3, withscores=True)
+		#self.redisConnection.zrevrangebyscore("bla", 4, 1, start=0, num=3, withscores=True)
 		#self.redis_con.zrevrangebyscore(config_global.dbname_rawJson + ":zset", float("infinity"), 0, 0, N, withscores = False)
 		return toplist
+
+	def make_plot(self):
+		''' '''
 
 
 
 if __name__ == '__main__':
-	us = UserStats('userid', 'itemid')
-	us.save(1,2)
-	us.save(1,3)
-
-	print us.get_Top_N('userid', 3)
+	''' '''
             
