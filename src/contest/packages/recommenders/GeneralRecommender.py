5c75d9bb557600a4ce72e2d98000d1fd7ec6e1d1
Christian Winkelmann <christian.winkelmann@plista.com>
2012-06-06 21:33:49 +0200

    draft for GeneralRecommender Superclass which orchestrates Ensemble Mixing and Queuing the training sessions later on

--- src/contest/packages/recommenders/GeneralRecommender.py
+++ src/contest/packages/recommenders/GeneralRecommender.py
@@ -5,27 +5,47 @@ Recommender which is recommends depending on the message the best item
 
 @author: christian.winkelmann@plista.com
 '''
-from contest.packages.recommenders.fallback_random import fallback_random
+from contest.packages.message_parsers.fullParser import FullContestMessageParser
+from contest.packages.recommenders.Random_Recommender import Random_Recommender
+
 
 class GeneralRecommender(object):
+    resultSet = None
 
-
-    def __init__(self, message):
+    def __init__(self, json_string, async=False, api='contest', backends=[]):
         '''
         Constructor
         '''
-        ## TODO parse the message to know what we are supposed to do
-        
+        self.backends = backends
+
+        fullParsedDataModel = FullContestMessageParser()
+        fullParsedDataModel.parse(json_string)
+
+        domain_id = fullParsedDataModel.domain_id
+        user_id = fullParsedDataModel.user_id
+        additional_filter = {'domainid': domain_id}
+
+        random_recommender = Random_Recommender()
+        N = 4
+        random_recommender.train(user_id, additional_filter)
+        self.resultSet = random_recommender.get_recommendation(user_id, additional_filter, N=N, remove=False)
+
+        # TODO parse the message to know what we are supposed to do: i.e. grab the constraints out of the data
+
+
+
+
+        # TODO initialize a new training session if necessary
+
     def recomend(self):
-        random_recommender = fallback_random()
-        list1 = random_recommender.getRecommendation()
-        
-        if ( len(list1) < 5 ):
-            random_recommender.train() 
-        
-    
-    
-        
-        
-    
-    
+        ''' lets hope there are recommendation ready for this user/constrain '''
+        return self.resultSet
+
+
+
+
+
+
+
+
+
