b7866ec998c66392b798222f28baecd25e3f6c77
Christian Winkelmann <christian.winkelmann@plista.com>
2012-06-11 21:39:33 +0200

    removed most of the code to General Recommender since it is just the base Class to real recommenders

--- src/contest/packages/recommenders/GeneralRecommender.py
+++ src/contest/packages/recommenders/GeneralRecommender.py
@@ -12,35 +12,14 @@ from contest.packages.recommenders.Random_Recommender import Random_Recommender
 class GeneralRecommender(object):
     resultSet = None
 
-    def __init__(self, json_string, async=False, api='contest', backends=[]):
+    def __init__(self, json_string, async=False, api='contest', backends=[], type='onsite'):
         '''
         Constructor
         '''
-        self.backends = backends
 
-        fullParsedDataModel = FullContestMessageParser()
-        fullParsedDataModel.parse(json_string)
-
-        domain_id = fullParsedDataModel.domain_id
-        user_id = fullParsedDataModel.user_id
-        
-        # now compile the constraints
-        constraints = {'domainid': domain_id}
-
-        random_recommender = Random_Recommender()
-        N = 4
-        random_recommender.train(user_id, constraints)
-        self.resultSet = random_recommender.get_recommendation(user_id, constraints, N=N, remove=True)
-
-        # TODO parse the message to know what we are supposed to do: i.e. grab the constraints out of the data
-
-
-
-
-        # TODO initialize a new training session if necessary
 
     def recommend(self):
-        ''' lets hope there are recommendation ready for this user/constrain '''
+        """ lets hope there are recommendation ready for this user/constraint """
         return self.resultSet
 
 
