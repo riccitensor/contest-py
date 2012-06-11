a93368b197b3007ea1f415e1f4316e26a2ec4af3
Christian Winkelmann <christian.winkelmann@plista.com>
2012-06-11 21:39:57 +0200

    adapted unit test to onsite and crosssite recommendation

--- src/contest/unitTests/integration/WorkFlowBase.py
+++ src/contest/unitTests/integration/WorkFlowBase.py
@@ -27,11 +27,23 @@ class WorkFlowBase():
             passwd=password, db="db_youfilter")
 
 
-    def mockMysqlResults(self, LIMIT=10):
+    def mockMysqlResults_domain418(self, LIMIT=10):
         # ( id, sth., sth. , json )
         message1 = "{\"msg\":\"impression\",\"id\":1300277,\"client\":{\"id\":1},\"domain\":{\"id\":\"418\"},\"item\":{\"id\":\"123\",\"title\":\"TEXT1\",\"url\":\"http:\/\/www.ksta.de\/html\/fotolines\/1321650273296\/rahmen.shtml?1\",\"created\":1321957664,\"text\":\"Talkshow-Mode\u00adra\u00adtorin Wendy Williams bei den 39. inter\u00adna\u00adtio\u00adnalen Emmy-Awards. (Bild: AFP)\",\"img\":null,\"recommendable\":true},\"config\":{\"timeout\":null,\"recommend\":true,\"limit\":1,\"team\":{\"id\":\"22\"}},\"version\":\"1.0\"}"
-        message2 = "{\"msg\":\"impression\",\"id\":1300277,\"client\":{\"id\":2},\"domain\":{\"id\":\"418\"},\"item\":{\"id\":\"456\",\"title\":\"TEXT2\",\"url\":\"http:\/\/www.ksta.de\/html\/fotolines\/1321650273296\/rahmen.shtml?1\",\"created\":1321957664,\"text\":\"Talkshow-Mode\u00adra\u00adtorin Wendy Williams bei den 39. inter\u00adna\u00adtio\u00adnalen Emmy-Awards. (Bild: AFP)\",\"img\":null,\"recommendable\":true},\"config\":{\"timeout\":null,\"recommend\":true,\"limit\":1,\"team\":{\"id\":\"22\"}},\"version\":\"1.0\"}"
-        message3 = "{\"msg\":\"impression\",\"id\":1300277,\"client\":{\"id\":3},\"domain\":{\"id\":\"418\"},\"item\":{\"id\":\"789\",\"title\":\"TEXT3\",\"url\":\"http:\/\/www.ksta.de\/html\/fotolines\/1321650273296\/rahmen.shtml?1\",\"created\":1321957664,\"text\":\"Talkshow-Mode\u00adra\u00adtorin Wendy Williams bei den 39. inter\u00adna\u00adtio\u00adnalen Emmy-Awards. (Bild: AFP)\",\"img\":null,\"recommendable\":true},\"config\":{\"timeout\":null,\"recommend\":true,\"limit\":1,\"team\":{\"id\":\"22\"}},\"version\":\"1.0\"}"
+        message2 = "{\"msg\":\"impression\",\"id\":1300278,\"client\":{\"id\":2},\"domain\":{\"id\":\"418\"},\"item\":{\"id\":\"456\",\"title\":\"TEXT2\",\"url\":\"http:\/\/www.ksta.de\/html\/fotolines\/1321650273296\/rahmen.shtml?1\",\"created\":1321957664,\"text\":\"Talkshow-Mode\u00adra\u00adtorin Wendy Williams bei den 39. inter\u00adna\u00adtio\u00adnalen Emmy-Awards. (Bild: AFP)\",\"img\":null,\"recommendable\":true},\"config\":{\"timeout\":null,\"recommend\":true,\"limit\":1,\"team\":{\"id\":\"22\"}},\"version\":\"1.0\"}"
+        message3 = "{\"msg\":\"impression\",\"id\":1300279,\"client\":{\"id\":3},\"domain\":{\"id\":\"418\"},\"item\":{\"id\":\"789\",\"title\":\"TEXT3\",\"url\":\"http:\/\/www.ksta.de\/html\/fotolines\/1321650273296\/rahmen.shtml?1\",\"created\":1321957664,\"text\":\"Talkshow-Mode\u00adra\u00adtorin Wendy Williams bei den 39. inter\u00adna\u00adtio\u00adnalen Emmy-Awards. (Bild: AFP)\",\"img\":null,\"recommendable\":true},\"config\":{\"timeout\":null,\"recommend\":true,\"limit\":1,\"team\":{\"id\":\"22\"}},\"version\":\"1.0\"}"
+
+        return (
+            (1, "metadata1", "metadata2", message1),
+            (2, "metadata1", "metadata2", message2),
+            (3, "metadata1", "metadata2", message3),
+            )
+
+    def mockMysqlResults_domain800(self, LIMIT=10):
+        # ( id, sth., sth. , json )
+        message1 = "{\"msg\":\"impression\",\"id\":1300280,\"client\":{\"id\":1},\"domain\":{\"id\":\"800\"},\"item\":{\"id\":\"999\",\"title\":\"TEXT1\",\"url\":\"http:\/\/www.ksta.de\/html\/fotolines\/1321650273296\/rahmen.shtml?1\",\"created\":1321957664,\"text\":\"Talkshow-Mode\u00adra\u00adtorin Wendy Williams bei den 39. inter\u00adna\u00adtio\u00adnalen Emmy-Awards. (Bild: AFP)\",\"img\":null,\"recommendable\":true},\"config\":{\"timeout\":null,\"recommend\":true,\"limit\":1,\"team\":{\"id\":\"22\"}},\"version\":\"1.0\"}"
+        message2 = "{\"msg\":\"impression\",\"id\":1300281,\"client\":{\"id\":2},\"domain\":{\"id\":\"800\"},\"item\":{\"id\":\"888\",\"title\":\"TEXT2\",\"url\":\"http:\/\/www.ksta.de\/html\/fotolines\/1321650273296\/rahmen.shtml?1\",\"created\":1321957664,\"text\":\"Talkshow-Mode\u00adra\u00adtorin Wendy Williams bei den 39. inter\u00adna\u00adtio\u00adnalen Emmy-Awards. (Bild: AFP)\",\"img\":null,\"recommendable\":true},\"config\":{\"timeout\":null,\"recommend\":true,\"limit\":1,\"team\":{\"id\":\"22\"}},\"version\":\"1.0\"}"
+        message3 = "{\"msg\":\"impression\",\"id\":1300282,\"client\":{\"id\":3},\"domain\":{\"id\":\"800\"},\"item\":{\"id\":\"777\",\"title\":\"TEXT3\",\"url\":\"http:\/\/www.ksta.de\/html\/fotolines\/1321650273296\/rahmen.shtml?1\",\"created\":1321957664,\"text\":\"Talkshow-Mode\u00adra\u00adtorin Wendy Williams bei den 39. inter\u00adna\u00adtio\u00adnalen Emmy-Awards. (Bild: AFP)\",\"img\":null,\"recommendable\":true},\"config\":{\"timeout\":null,\"recommend\":true,\"limit\":1,\"team\":{\"id\":\"22\"}},\"version\":\"1.0\"}"
 
         return (
             (1, "metadata1", "metadata2", message1),
@@ -44,7 +56,7 @@ class WorkFlowBase():
         '''
         '''
         if self.debug:
-            return self.mockMysqlResults(start_id=0, end_id=None, limit=None, type='impression')
+            return self.mockMysqlResults_domain418(start_id=0, end_id=None, limit=None, type='impression')
         else:
             self.db.query("""SELECT id, type, timestamp, json, response_time, response_id FROM contest.contest_messages c \
                 WHERE ( type = 'impression' ) AND LIMIT 10000 """)
