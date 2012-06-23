905987701a4f56fb023a25a5be5cecf866e14c8f
Christian Winkelmann <christian.winkelmann@plista.com>
2012-06-23 22:28:42 +0200

    testing sparse vector algebra

---
+++ src/test_scripts/matrix/vector_outer_product.py
@@ -0,0 +1,39 @@
+import numpy
+from numpy.matrixlib.defmatrix import mat
+import scipy.linalg
+from scipy.sparse import lil_matrix
+from scipy.sparse.linalg import spsolve
+from numpy.linalg import solve, norm
+from numpy.random import rand
+
+
+v1 = lil_matrix((1, 10)) # create Row-based linked list sparse matrix
+v1[0,0] = 1
+v1[0,1] = 1
+v1_transpose = v1.transpose()
+
+A = v1_transpose * v1
+print "A"
+print A
+
+
+v2 = lil_matrix((1, 10)) # create Row-based linked list sparse matrix
+v2[0,0] = 1
+v2[0,1] = 1
+v2_transpose = v2.transpose()
+B = v2_transpose * v2
+print "B"
+print B
+
+print "A + B"
+print A + B
+
+
+v3 = lil_matrix((1, 10)) # create Row-based linked list sparse matrix
+v3[0,0] = 1
+v3[0,1] = 1
+v3_transpose = v3.transpose()
+C = v3_transpose * v3
+print C
+
+#print A[1, :]
